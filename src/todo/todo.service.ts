d79c8d8d98c2ef1351ea67021a3f63e20fbc8f2a
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-06-18 14:25:04 +0900

    Created getAllTodo()

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  private todo = [];
+
+  getAllTodo() {
+    return this.todo;
+  }
+}
