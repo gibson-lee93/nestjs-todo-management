aa80c5c5df0689b52592b21c413c053f4b13ea6f
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-06-30 00:46:07 +0900

    Guarded Todo routes using AuthGuard

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { TodoController } from './todo.controller';
 import { TodoRepository } from './todo.repository';
 import { TodoService } from './todo.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoRepository])],
+  imports: [TypeOrmModule.forFeature([TodoRepository]), AuthModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
